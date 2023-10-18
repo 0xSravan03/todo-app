a0984a7045d4bf19c4199131ead2fdc157e1617b
sravan <sravantp03@gmail.com>
2023-10-18 19:25:15 +0530

    added helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,21 @@ function App() {
     setTodos((prev) => [...prev, {id: Date.now(), ...todo}])
   }
 
+  /* This function is used to update the todo text */
   function updateTodo(id, newTodo) {
     setTodos(prev => prev.map(todo => todo.id == id ? newTodo : todo))
   }
 
-  function deleteTodo() {
-
+  function deleteTodo(id) {
+    setTodos(prev => prev.filter((todo) => todo.id != id))
   }
 
-  function toggleTodo() {
-
+  function toggleTodo(id) {
+    setTodos((prev) => prev.map(todo => todo.id == id ? {...todo, completed : true} : todo))
   }
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo}}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodo}}>
       <main className="w-full min-h-screen bg-slate-500 py-8">
         <section className="w-full max-w-2xl mx-auto border p-4 text-white">
           <h1 className="text-2xl font-bold text-center mt-2 mb-6">Manage Your Todos</h1>

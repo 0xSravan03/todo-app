2f0652b717f83be68fe90ac5226e66ee51577ccf
sravan <sravantp03@gmail.com>
2023-10-19 10:41:59 +0530

    made TodoItem component to render todos

--- src/App.jsx
+++ src/App.jsx
@@ -9,44 +9,57 @@ function App() {
     This function is actually used inside the todo form.
   */
   function addTodo(todo) {
-    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   }
 
   /* This function is used to update the todo text */
   function updateTodo(id, newTodo) {
-    setTodos(prev => prev.map(todo => todo.id == id ? newTodo : todo))
+    setTodos((prev) => prev.map((todo) => (todo.id == id ? newTodo : todo)));
   }
 
   function deleteTodo(id) {
-    setTodos(prev => prev.filter((todo) => todo.id != id))
+    setTodos((prev) => prev.filter((todo) => todo.id != id));
   }
 
   function toggleTodo(id) {
-    setTodos((prev) => prev.map(todo => todo.id == id ? { ...todo, completed : !todo.completed } : todo))
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id == id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
   }
 
   /** This will run only once when we load the application */
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('todos'));
+    const data = JSON.parse(localStorage.getItem("todos"));
 
     if (data && data.length > 0) {
-      setTodos(data)
+      setTodos(data);
     }
   }, []);
 
   /** This will run when we add new todo items and 
       store the updated todos in the local storage */
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodo}}>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleTodo }}
+    >
       <main className="w-full min-h-screen py-8 bg-[#172842]">
         <section className="w-full max-w-2xl mx-auto border p-4 text-white">
-          <h1 className="text-2xl font-bold text-center mt-2 mb-6">Manage Your Todos</h1>
+          <h1 className="text-2xl font-bold text-center mt-2 mb-6">
+            Manage Your Todos
+          </h1>
           <section>
             <TodoForm />
+          </section>
+          <section className="mt-10">
+            {todos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
           </section>
         </section>
       </main>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,73 @@
-function TodoItem() {
+import { useState, useEffect } from "react";
+import { useTodoContext } from "../contexts/TodoContext";
+
+function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMessage, setTodoMessage] = useState(todo.todo);
+  const { toggleTodo, deleteTodo, updateTodo } = useTodoContext();
+
+  function editTodo() {
+    updateTodo(todo.id, { ...todo, todo: todoMessage });
+    setIsTodoEditable(false);
+  }
+
   return (
-    <div>TodoItem</div>
-  )
+    <div className="w-full px-3">
+      <div
+        className={`w-full flex justify-between rounded-lg mb-4 px-6 py-4 text-black text-md ${
+          todo.completed ? `bg-green-300` : `bg-slate-400`
+        }`}
+        key={todo.id}
+      >
+        <div className={`flex items-center gap-2 w-full`}>
+          <input
+            type="checkbox"
+            className={`cursor-pointer w-4 h-4 hover:scale-110 duration-200`}
+            onChange={() => {
+              if (isTodoEditable) return;
+              toggleTodo(todo.id);
+            }}
+            checked={todo.completed}
+          />
+
+          {/* Place used for rendering todo text */}
+          <input
+            type="text"
+            spellCheck="false"
+            value={todoMessage}
+            className={`w-full mr-10 outline-none bg-transparent ${
+              todo.completed ? `line-through` : ``
+            } ${isTodoEditable ? "shadow" : ""}`}
+            readOnly={!isTodoEditable}
+            onChange={(e) => setTodoMessage(e.target.value)}
+          />
+        </div>
+        <div className="flex gap-4">
+          <button
+            className={`inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 hover:scale-105 duration-100`}
+            onClick={() => {
+              if (todo.completed) return;
+
+              if (isTodoEditable) {
+                editTodo();
+              } else {
+                setIsTodoEditable(!isTodoEditable);
+              }
+            }}
+            disabled={todo.completed}
+          >
+            {!isTodoEditable ? "✏️" : "📁"}
+          </button>
+          <button
+            className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 hover:scale-105 duration-100"
+            onClick={(e) => deleteTodo(todo.id)}
+          >
+            ❌
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
